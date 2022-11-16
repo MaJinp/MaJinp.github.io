f9b4c9c08e19221d3d12f38ab0df9c7266124e11
Majinp <983194925@qq.com>
2022-11-17 00:52:17 +0800

    Site updated: 2022-11-17 00:52:16

---
+++ js/custom/cheesy_pick-up_lines.js
@@ -0,0 +1,17 @@
+function cheesy_pick_up_lines() {
+    let html = "";
+    for (let i = 0; i < 3; i++) {
+      fetch("https://api.vvhan.com/api/love")
+        .then((data) => data.text())
+        .then((data) => {
+          html += "<li>" + data + "</li>";
+          document.querySelector("#cheesy_pick-up_lines-container").innerHTML =
+            html;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+  
+  cheesy_pick_up_lines();
